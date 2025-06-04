cc20f7e62c3568c8088b36b5b018a9f594eaaf2b
devjiwonchoi <devjiwonchoi@gmail.com>
2025-06-04 20:09:56 +0200

    init2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode, startTransition } from 'react'
 import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document)
@@ -10,8 +10,23 @@ root.render(
 )
 
 const script = document.createElement('script')
+
+script.style.display = 'block'
+script.style.position = 'absolute'
+script.setAttribute('data-nextjs-dev-overlay', 'true')
+
+const container = document.createElement('nextjs-portal')
+container.attachShadow({ mode: 'open' })
+script.appendChild(container)
+
 document.body.appendChild(script)
 
-const nextjsPortal = document.createElement('nextjs-portal')
-const shadowRoot = nextjsPortal.attachShadow({ mode: 'open' })
-script.appendChild(nextjsPortal)
+const devToolsRoot = createRoot(container, {
+  identifierPrefix: 'ndt-',
+})
+
+devToolsRoot.render(<DevTools />)
+
+function DevTools() {
+  return <p>dev overlay</p>
+}
